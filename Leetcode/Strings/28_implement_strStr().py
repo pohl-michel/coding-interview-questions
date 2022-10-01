6149b4e0aaba734c22aaf6316d7077ff5e71bba8
Michel Pohl <michel.pohl.20.08@gmail.com>
2022-10-01 16:36:45 +0900

    Added comments to implement_strStr() and added .py file extension

--- Leetcode/Strings/28_implement_strStr().py
+++ Leetcode/Strings/28_implement_strStr().py
@@ -21,3 +21,6 @@ for haystack, needle in examples:
 
 # Rk 1: "if haystack[i_start:(i_start+m)] == needle" could be replaced by an inner loop
 # Rk 2: the Knuth-Morris-Pratt algorithm: https://www.youtube.com/watch?v=JoF0Z7nVSrA
+# Basically reinventing the KMP algorithm in itself is very hard, but one should be able to:
+# - efficiently implement code to find the longest prefix suffix (LPS)
+# - understand how that brick of code helps find matching substrings
